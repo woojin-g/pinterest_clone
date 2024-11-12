e2f120b8d4304bfb7967d8d3dd39402d0cfa9af5
woojin-g <blueofficial0119@gmail.com>
2024-11-13 03:07:16 +0900

    db 연동

--- account_app/views.py
+++ account_app/views.py
@@ -1,7 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from account_app.models import HelloWorld
+
 
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'account_app/hello_world.html')
+  if request.method == 'POST':
+    text = request.POST.get('hello_world_input')
+
+    model = HelloWorld()
+    model.text = text
+    model.save()
+
+    models = HelloWorld.objects.all()
+
+    return HttpResponseRedirect(reverse('account_app:hello_world'))
+
+  elif request.method == 'GET':
+    models = HelloWorld.objects.all()
+    return render(request,
+                  'account_app/hello_world.html',
+                  context={
+                    'outputs': models
+                  })
