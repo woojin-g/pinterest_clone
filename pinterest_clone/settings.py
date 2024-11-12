bdeac4107ce671da77391f90d66891efdb6f25fe
woojin-g <blueofficial0119@gmail.com>
2024-11-13 01:41:29 +0900

    css 추가

--- pinterest_clone/settings.py
+++ pinterest_clone/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.conf.global_settings import STATIC_ROOT, STATICFILES_DIRS
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -129,6 +131,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
