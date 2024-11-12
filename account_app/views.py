f8d609ebb1305d33de40f51abb243d21fd9dcfc7
woojin-g <blueofficial0119@gmail.com>
2024-11-13 00:02:44 +0900

    html template 생성

--- account_app/views.py
+++ account_app/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'base.html')
+    return render(request, 'account_app/hello_world.html')
